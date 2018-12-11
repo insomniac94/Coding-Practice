1ca3edcb088cf774f36bb484f96f36acc8bc393f
insomniac94 <41095722+insomniac94@users.noreply.github.com>
2018-12-11 14:42:08 -0500

    second commit

--- card.py
+++ card.py
@@ -1,15 +1,5 @@
 import re
 
-# What kind of card is it?
-
-# Visa - starts with a 4 (up to 19 digits)
-
-# Mastercard - (16 digits) First digit must be 5 and second digit must be in the range 1 through 5 inclusive. The range is 510000 through 559999
-#              First digit must be 2 and second digit must be in the range 2 through 7 inclusive. The range is 222100 through 272099
-
-# Discover - (16 digits) First 6 digits must be in one of the following ranges: 601100 through 601109, 601120 through 601149, 601174, 601177 through 601179, 
-#            601186 through 601199, or 644000 through 659999 
-
 cardNumberInput = input("Enter a card number: ")
 regEx = r'\d{1}'
 pattern = re.compile(regEx)
