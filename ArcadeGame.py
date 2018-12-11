bb6d49d4a68a8a18ab920860a5536a6eb407f730
insomniac94 <41095722+insomniac94@users.noreply.github.com>
2018-12-11 18:21:40 -0500

    sixth commit

--- ArcadeGame.py
+++ ArcadeGame.py
@@ -13,11 +13,14 @@ pygame.init()
 WIDTH = 800
 HEIGHT = 600
 
+RESULT_SCREEN_WIDTH = 350 
+RESULT_SCREEN_HEIGHT = 200
+
 BLACK = (0,0,0)
 RED = (255,0,0)
 BLUE = (0,0,255)
 WHITE = (255,255,255)
-BACKGROUND_COLOR = (11,238,207)
+BACKGROUND_COLOR = (133,79,249)
 
 speed = 10
 
@@ -37,10 +40,15 @@ game_over = False
 
 clock = pygame.time.Clock()
 
-myFont = pygame.font.SysFont("Courier New", 35)
+myFont = pygame.font.SysFont("Courier New", 25)
+
+def print_results(score):
+	result_screen = pygame.display.set_mode((RESULT_SCREEN_WIDTH, RESULT_SCREEN_HEIGHT))
+	results = "Score:" + str(score)
+	label = myFont.render(results, 1, BLACK)
+	result_screen.blit(label, (WIDTH/2,HEIGHT/2))
 
 def level_difficulty(score, speed):
-
 	#speed will be proportional to score
 	speed = score / 10 + 3
 	return speed
@@ -48,7 +56,7 @@ def level_difficulty(score, speed):
 def drop_enemies(enemy_list):
 	delay = random.random()
 	if len(enemy_list) < 10 and delay < 0.1:
-		x_pos = randomPos
+		x_pos = random.randint(0, WIDTH - square_size)
 		y_pos = 0
 		enemy_list.append([x_pos, y_pos])
 
@@ -71,7 +79,6 @@ def collision_check(enemy_list, user_pos):
 			return True
 	return False
 
-
 def collision_detection(user_pos,enemy_pos):
 	user_x = user_pos[0]
 	user_y = user_pos[1]
@@ -85,13 +92,11 @@ def collision_detection(user_pos,enemy_pos):
 	return False
 
 while not game_over:
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
 		if event.type == pygame.KEYDOWN:
-
 			x = user_pos[0]
 			y = user_pos[1]
 
@@ -103,23 +108,30 @@ while not game_over:
 
 			user_pos = [x,y]
 
+	pygame.display.set_caption('Brandons Arcade Game')
 	screen.fill(BACKGROUND_COLOR)
 
 	if collision_detection(user_pos, enemy_pos):
 		game_over = True
-		#break will show collision on screen
+
 
 	drop_enemies(enemy_list)
 	score = update_enemy_pos(enemy_list, score)
 	speed = level_difficulty(score, speed)
 
-	text = "Score: " + str(score)
+	text = "Score:" + str(score)
 	label = myFont.render(text, 1, BLACK)
-	screen.blit(label, (WIDTH-200,HEIGHT-40))
+	screen.blit(label, (WIDTH-180,HEIGHT-40))
 
 	if collision_check(enemy_list, user_pos):
+		displayResults = True
 		game_over = True
-		break
+
+		while displayResults:
+			print_results(score)
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
 
 	draw_enemies(enemy_list)
 
